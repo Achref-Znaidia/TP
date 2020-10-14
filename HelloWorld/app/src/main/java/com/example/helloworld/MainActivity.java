0eca774858592eb0f181bc83295c6ddf8dbab5cb
Achref-Znaidia <72569987+Achref-Znaidia@users.noreply.github.com>
2020-10-14 20:32:54 +0200

    Add files via upload

---
+++ HelloWorld/app/src/main/java/com/example/helloworld/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.helloworld;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Log.d("Question1","activity_main.xml");
+        Log.d("Question2","app_name");
+        Log.d("Question3","AVD Manager");
+        Log.d("Question4","Info");
+    }
+}
